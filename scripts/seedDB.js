b984f10d5c498f46daf223c890613ffded0b7ac6
Coleman Buffa <coleman.buffa@gmail.com>
2021-03-03 15:50:44 -0800

    Update props key value pair passed from searchcontainer to searchcard.

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const db = require("../models");
 
 // This file empties the Books collection and inserts the books below
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+mongoose.connect("mongodb+srv://admin:56uzzPUYmOCw7PYS@cluster0.ctfd0.mongodb.net/react-books");
 
 const bookSeed = [
 	{
